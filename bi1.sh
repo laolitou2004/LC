1db1eb3eb142a1a1306dadadf49d8f5df9a7633f
laolitou2004 <143444972+laolitou2004@users.noreply.github.com>
2024-07-28 19:38:01 +0800

    Add files via upload

---
+++ bi1.sh
@@ -0,0 +1,1 @@
+sudo /home/lio/a/child1/xclash -f /home/lio/a/child1/xclash.yaml -d /home/lio/a/child1 -ext-ui /home/lio/a/child1/yacd
---
+++ child1/official/assets/global-mJZNxgPZ.js
@@ -0,0 +1,1 @@
+import{Y as a}from"./vendor-DW_s8V88.js";import{ad as m}from"./index-Bmioxl3y.js";const s=o=>a(o).locale(m()).fromNow();export{s as f};
