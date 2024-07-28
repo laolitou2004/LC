ba713498e7fda4cf5aea1cefa6221d92808644e4
laolitou2004 <143444972+laolitou2004@users.noreply.github.com>
2024-07-28 19:53:43 +0800

    Add files via upload

---
+++ child1/ipshengc.sh
@@ -0,0 +1,12 @@
+cd ~/a生产/cfip
+sudo ./cfip.sh
+cp ~/a生产/cfip/ip.txt ~/a生产/zip写入/ip.txt
+cd ~/a生产/zip写入
+zsh ~/a生产/zip写入/ip写入cl.sh
+cp xclash.yaml1 xclash.yaml
+cat a.txt >> xclash.yaml
+cp xclash.yaml ~/child1/xclash.yaml
+rm xclash.yaml
+cd ~/child1
+tmux a
+cat ~/a生产/zip写入/010204.sh
---
+++ child1/ipshengc1.sh
@@ -0,0 +1,10 @@
+cp ~/a生产/cfip/ip.txt ~/a生产/zip写入/ip.txt
+cd ~/a生产/zip写入
+zsh ~/a生产/zip写入/ip写入cl.sh
+cp xclash.yaml1 xclash.yaml
+cat a.txt >> xclash.yaml
+cp xclash.yaml ~/child1/xclash.yaml
+rm xclash.yaml
+cd ~/child1
+tmux a
+cat ~/a生产/zip写入/010204.sh
